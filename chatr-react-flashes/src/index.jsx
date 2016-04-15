3d036a1c8519d6cf68e63a592b34d7f0fbb406c4
Lubos Belak <belak.lubos@gmail.com>
2016-04-15 08:23:35 +0200

    Changing state {} + Adding if statement for the height

--- chatr-react-flashes/src/index.jsx
+++ chatr-react-flashes/src/index.jsx
@@ -3,43 +3,44 @@ import {render} from 'react-dom';
 import {Observable} from 'rx';
 import axios from 'axios';
 
-const API = 'https://chatr.tv/api/flashes/0';
-const p = axios.get(API);
+const URL = 'https://chatr.tv/api/flashes/0';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      flash: {}
-    };
+    this.state = { flash: {} };
   }
 
   componentWillMount() {
     Rx.Observable
       .interval(1000)
-      .merge(Rx.Observable.of(0))
-      .flatMap(() => Rx.Observable.fromPromise(axios.get(API)))
+      .flatMap(() => Rx.Observable.fromPromise(axios.get(URL)))
       .filter(response => response.status === 200)
-      .map(result => result.data)
-      .subscribe(res => this.setState({ flash: res.flash }));
+      .map(result => result.data.flash)
+      .subscribe(data => this.setState({ flash: data }));
   }
 
   render() {
     let flash = this.state.flash;
 
+    if (flash) {
+      const stream = document.querySelector('#stream');
+      stream.classList.add('🔘');
+    }
+
     return (
-      <div className="⚡">
-        <div className="🔥">
-          <div className="💬">{flash.text}</div>
+      <div className='⚡'>
+        <div className='🔥'>
+          <div className='💬'>{flash.text}</div>
         </div>
-        <div className="↕">
-          <div className="👍" style={{height: flash.choice_a_percent + "vh"}}>
+        <div className='↕'>
+          <div className='👍' style={(flash.choice_a_percent === 0 && flash.choice_b_percent === 0) ? {height: '50vh'} : {height: flash.choice_a_percent + 'vh'}}>
             <div>{flash.choice_a}</div>
-            <div className="🎈">{flash.choice_a_percent}</div>
+            <div className='🎈'>{flash.choice_a_percent}</div>
           </div>
-          <div className="👎" style={{height: flash.choice_b_percent + "vh"}}>
+          <div className='👎' style={(flash.choice_a_percent === 0 && flash.choice_b_percent === 0) ? {height: '50vh'} : {height: flash.choice_b_percent + 'vh'}}>
             <div>{flash.choice_b}</div>
-            <div className="🎈">{flash.choice_b_percent}</div>
+            <div className='🎈'>{flash.choice_b_percent}</div>
           </div>
         </div>
       </div>
