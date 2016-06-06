4468dabbcc50c9d0f72291595b93014c2b178376
Lubos Belak <belak.lubos@gmail.com>
2016-06-06 21:10:58 +0200

    Adding webpack file loader and importing libraries

---
+++ tap-clap/src/index.js
@@ -0,0 +1,5 @@
+import '../../style.css';
+
+import { run }                              from '@cycle/xstream-run';
+import { makeDOMDriver, div }               from '@cycle/dom';
+import xs                                   from 'xstream';
