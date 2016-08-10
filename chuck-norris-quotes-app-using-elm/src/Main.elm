9a4fbb14bd70944d885e3f54bfd99d687774da2c
Lubos Belak <belak.lubos@gmail.com>
2016-08-10 21:28:16 +0200

    Formatting the Elm code & Adding .gitignore file

--- chuck-norris-quotes-app-using-elm/src/Main.elm
+++ chuck-norris-quotes-app-using-elm/src/Main.elm
@@ -1,3 +1,5 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.App as Html
 import Html.Events exposing (..)
@@ -5,30 +7,34 @@ import Html.Attributes exposing (..)
 import Http
 import Task exposing (Task)
 
+
 {-
-  MODEL
-  * Model type
-  * Initialize model with empty values
+   MODEL
+   * Model type
+   * Initialize model with empty values
 -}
+
 
 type alias Model =
-  { quote : String
-  }
+    { quote : String
+    }
 
-init : (Model, Cmd Msg)
+
+init : ( Model, Cmd Msg )
 init =
-  ( Model "", fetchRandomQuoteCmd )
+    ( Model "", fetchRandomQuoteCmd )
+
 
 
 {-
-  UPDATE
-  * API Routes
-  * GET
-  * Messages
-  * Update Case
+   UPDATE
+   * API Routes
+   * GET
+   * Messages
+   * Update Case
 -}
-
 -- API request URLs
+
 
 api : String
 api =
@@ -37,58 +43,65 @@ api =
 
 randomQuoteUrl : String
 randomQuoteUrl =
-  api ++ "api/random-quote"
+    api ++ "api/random-quote"
+
+
 
 -- GET a random quote
 
+
 fetchRandomQuote : Platform.Task Http.Error String
 fetchRandomQuote =
-  Http.getString randomQuoteUrl
+    Http.getString randomQuoteUrl
 
 
 fetchRandomQuoteCmd : Cmd Msg
 fetchRandomQuoteCmd =
-  Task.perform HttpError FetchQuoteSuccess fetchRandomQuote
+    Task.perform HttpError FetchQuoteSuccess fetchRandomQuote
 
 
 type Msg
-  = GetQuote
-  | FetchQuoteSuccess String
-  | HttpError Http.Error
+    = GetQuote
+    | FetchQuoteSuccess String
+    | HttpError Http.Error
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    GetQuote ->
-      ( model, fetchRandomQuoteCmd )
+    case msg of
+        GetQuote ->
+            ( model, fetchRandomQuoteCmd )
 
-    FetchQuoteSuccess newQuote ->
-      ( { model | quote = newQuote }, Cmd.none )
+        FetchQuoteSuccess newQuote ->
+            ( { model | quote = newQuote }, Cmd.none )
 
-    HttpError _ ->
-      ( model, Cmd.none )
+        HttpError _ ->
+            ( model, Cmd.none )
+
+
 
 {-
-  VIEW
+   VIEW
 -}
+
 
 view : Model -> Html Msg
 view model =
-  div [ class "container" ]
-    [ h2 [ class "text-center" ] [ text "Chuck Norris Quotes" ]
-    , p [ class "text-center" ]
-      [ button [ class "btn btn-success", onClick GetQuote ] [ text "Grab a quote!" ] ]
-    , blockquote [] [
-      p [] [ text model.quote ]
-    ]
-  ]
+    div [ class "container" ]
+        [ h2 [ class "text-center" ] [ text "Chuck Norris Quotes" ]
+        , p [ class "text-center" ]
+            [ button [ class "btn btn-success", onClick GetQuote ] [ text "Grab a quote!" ] ]
+        , blockquote []
+            [ p [] [ text model.quote ]
+            ]
+        ]
 
 
 main : Program Never
 main =
     Html.program
-      { init = init
-      , update = update
-      , subscriptions = \_ -> Sub.none
-      , view = view
-      }
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        }
