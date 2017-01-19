1e407d674b2c58450cb1ff5328751bae6419ca22
Lubos Belak <belak.lubos@gmail.com>
2017-01-19 17:29:27 +0100

    Adding Redux and creating Redux store

--- pwa/enjoi/src/elements/stage-list/stage-list.js
+++ pwa/enjoi/src/elements/stage-list/stage-list.js
@@ -1,4 +1,23 @@
+import { createStore } from 'redux';
+
 const DATA_URL = '../data/data.json'
+
+const initialState = {
+    artists: []
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INIT_ARTISTS':
+            const artists = state.artists.slice(0);
+            return Object.assign({}, ...state, { artists: action.artists });
+
+        default:
+            return state;
+    }
+};
+
+const store = createStore(reducer);
 
 export class StageList extends HTMLElement {
     constructor() {
@@ -11,17 +30,28 @@ export class StageList extends HTMLElement {
 
     connectedCallback() {
         this._fetchArtists(DATA_URL);
+
+        store.subscribe(_ => {
+            this.artists = store.getState().artists;
+            if (!this.artists) return;
+            this.render();
+        });
     }
 
     _fetchArtists(url) {
         fetch(url)
             .then(response => response.json())
             .then(json => this.artists = json.map(d => d.artists))
-            .then(artists => this.render(this.artists))
+            .then(artists => {
+                store.dispatch({
+                    type: 'INIT_ARTISTS',
+                    artists: [...artists]
+                });
+            })
             .catch(error => console.error('💩', error));
     }
 
-    render(artists) {
-            this.innerHTML = `${artists.map(artist => `<stage-artists artists='${JSON.stringify(artist)}'></stage-artist>`).join('')}`;
+    render() {
+            this.innerHTML = `${this.artists.map(artist => `<stage-artists artists='${JSON.stringify(artist)}'></stage-artist>`).join('')}`;
     }
 }
